579526246d46679ee6a47fc8f1fbc9be962e1913
Viacheslav Shulha <viacheslav.shulha@lanars.com>
2025-03-02 16:17:38 +0200

    Create UIKit template

---
+++ MapDownloader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapDownloader
+//
+//  Created by Viacheslav on 02.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
